7f49fe0a5475b3377cd718cb3580a8e678183e06
onlymac <nirbhaygoswami789@gmail.com>
2024-02-07 15:37:45 +0530

    do ui changes

---
+++ lib/util/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages
+{
+  static const String path="asset/images";
+
+  static String logo="$path/logo_without_title.png";
+  static String logoWithTitle="$path/logo.png";
+}
---
+++ lib/util/color_utils.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorUtils
+{
+ static var primaryColor=Color(0xffF2A600);
+ static var white=Colors.white;
+ static var black=Colors.black;
+
+}
